951646a5fc52c446a8d72e123d4d43eda39b95b6
Tahir Uzun <tahirtall46@gmail.com>
2020-03-10 08:07:37 -0500

    new homework

---
+++ TahirUzun_Assignment8/TahirUzun_Assignment8/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TahirUzun_Assignment8
+//
+//  Created by Tahir Uzun on 3/10/20.
+//  Copyright © 2020 Tahir Uzun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
