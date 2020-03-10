b9aea59f4229c0dc9abf37d14a36df4dfe815c96
Tahir Uzun <tahirtall46@gmail.com>
2020-03-10 10:23:45 -0500

    Created visual look, and wrote down the riddles. Now, write the viewcontrollers

---
+++ TahirUzun_Assignment8/TahirUzun_Assignment8/AnswerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  AnswerViewController.swift
+//  TahirUzun_Assignment8
+//
+//  Created by Tahir Uzun on 3/10/20.
+//  Copyright © 2020 Tahir Uzun. All rights reserved.
+//
+
+import Foundation
